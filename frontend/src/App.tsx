5b71b81076fff8cbb1eb4391428217b6b6665498
anisberto reis <anisbertoos@gmail.com>
2022-07-15 16:59:28 -0300

    Create first component - button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
   return (
-    <h1>Ola mundo</h1>
+    <>
+      < NotificationButton />
+      < NotificationButton />
+    </>
   )
 }
 
-export default App
+export default App;
