2b2df128bff3a8f7d6a61dcd00be12b226c20856
anisberto reis <anisbertoos@gmail.com>
2022-07-16 23:34:15 -0300

    Inclusão do swagger para documentação da API

--- backend/src/main/java/br/com/anisberto/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/br/com/anisberto/dsmeta/DsmetaApplication.java
@@ -2,12 +2,14 @@ package br.com.anisberto.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DsmetaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsmetaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DsmetaApplication.class, args);
+    }
 
 }
--- backend/src/main/java/br/com/anisberto/dsmeta/configuration/SecurityConfig.java
+++ backend/src/main/java/br/com/anisberto/dsmeta/configuration/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableWebSecurity
@@ -35,4 +40,14 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
