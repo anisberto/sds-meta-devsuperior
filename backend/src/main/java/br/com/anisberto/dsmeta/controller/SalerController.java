6ad79217f6ac00427e9e853541598c38aeb78b70
anisberto reis <anisbertoos@gmail.com>
2022-07-17 01:24:24 -0300

    sms - message configuration

--- backend/src/main/java/br/com/anisberto/dsmeta/controller/SalerController.java
+++ backend/src/main/java/br/com/anisberto/dsmeta/controller/SalerController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("saler")
@@ -35,8 +32,8 @@ public class SalerController {
         return ResponseEntity.ok(salerService.getAllSalers(minData, maxData, pageable));
     }
 
-    @GetMapping("/notification")
-    public void nofitySaler() {
-        smsService.sendSms();
+    @GetMapping("{id}/notification")
+    public void nofitySaler(@PathVariable("id") Integer id) {
+        smsService.sendSms(id);
     }
 }
--- backend/src/main/java/br/com/anisberto/dsmeta/service/SmsService.java
+++ backend/src/main/java/br/com/anisberto/dsmeta/service/SmsService.java
@@ -1,8 +1,11 @@
 package br.com.anisberto.dsmeta.service;
 
+import br.com.anisberto.dsmeta.entities.Sale;
+import br.com.anisberto.dsmeta.repository.SalerRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +23,34 @@ public class SmsService {
     @Value("${twilio.phone.to}")
     private String twilioPhoneTo;
 
+    @Autowired
+    private SalerRepository salerRepository;
+
     public void sendSms() {
         Twilio.init(twilioSid, twilioKey);
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
         PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
         Message message = Message.creator(to, from, "SDS - ANISBERTO - TESTE").create();
         System.out.println(message.getSid());
+    }
+
+    public void sendSms(Integer id) {
+        Sale sale = salerRepository.findById(id.longValue()).get();
+        Twilio.init(twilioSid, twilioKey);
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+        Message message = Message.creator(to, from, contruirMensagem(sale)).create();
+        System.out.println(message.getSid());
+    }
+
+    public String contruirMensagem(Sale sale){
+        StringBuffer db = new StringBuffer();
+        db.append("O vendedor: ");
+        db.append(sale.getSellerName());
+        db.append(" e de ID: " + sale.getId());
+        db.append("\nTotal Visited: " + sale.getVisited());
+        db.append(String.format("\nSomou um total de R$: %.2f" ,sale.getAmount()) +
+                ", Foi destaque em: " + sale.getDate());
+        return db.toString();
     }
 }
